e83c85e738443ecf643ff7bc979722cdcce2ab38
Gabrielle Beck <becgabri@jhu.edu>
2021-10-07 15:46:06 -0400

    initial commit

---
+++ code/main.py
@@ -0,0 +1,21 @@
+
+from HIBE.hibenc_lew11 import HIBE_LW11 
+import timelockpuzzle
+
+
+class TimeDeniableSig:
+
+    def KeyGen(self, timeGap, secParam):
+        return
+ 
+    def Sign(sk, m, t):
+        return
+
+    def AltSign(vk, m_0, t_0, sigma_0, m, t):
+        return
+
+    def Verify(vk, sigma, m, t):
+        return
+
+if __name__ == "__main__":
+    print("Trying to build a thing") 
