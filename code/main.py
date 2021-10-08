57564ec8db1b1f98a32fc5084a774f4e47f8e8eb
Gabrielle Beck <becgabri@jhu.edu>
2021-10-08 00:49:11 -0400

    small fixes, will do more later

--- code/main.py
+++ code/main.py
@@ -1,10 +1,16 @@
 
 from HIBE.hibenc_lew11 import HIBE_LW11 
 import timelockpuzzle
+import math
 
 # this is the size of time stamps supported
 TIME_SIZE_L = 4
 
+
+def FSDelegate(pk, t, sk_t, t_prime):
+    _, list_sk_t = sk_t 
+    new_list = [] 
+    return
 
 def FSKeygen(sk_prime, t):
     pk, sk = sk_prime 
@@ -15,15 +21,16 @@ def FSKeygen(sk_prime, t):
         curr_slice = t_as_bits[:len(t_as_bits)-1*i]
         is_LC = True if (curr_slice[len(curr_slice)-1] == '0') else False
         if is_LC and not find_right:
-            #deleg_key = HIBE_LW11.delegate(pk, sk, curr_slice)
+            #deleg_key = HIBE_LW11.keyGen(curr_slice, sk, pk)
             list_keys.append(curr_slice)
             find_right = True
         elif find_right and not is_LC: # means right child
             # take the left child of your parent
             identity = curr_slice[:-1] + '0'
-            #deleg_key = HIBE_LW11.delegate(pk, sk, identity)
+            #deleg_key = HIBE_LW11.keyGen(identity, sk, pk)
             list_keys.append(identity)
         elif len(curr_slice) == 1 and not is_LC:
+            #deleg_key = HIBE_LW11.keyGen('0', sk, pk)
             list_keys.append('0')  
     return list_keys 
 
